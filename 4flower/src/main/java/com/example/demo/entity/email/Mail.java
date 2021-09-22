c35c6b7a2f461e113cbfcac562f69769d6efe034
Jinhyunly <dbdbddipp@gmail.com>
2021-09-23 02:05:44 +0900

    add mail

---
+++ 4flower/src/main/java/com/example/demo/entity/email/Mail.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity.email;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Mail {
+
+		String mailTo;
+
+		String mailFrom;
+		String fromName;
+		String phoneNum;
+		String mailSubject;
+		String mailContent;
+}
